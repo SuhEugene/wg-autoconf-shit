ceb12136be36757b9ff1aa0b550b7a45d3c9e793
Eugene Suharev <e.sukharev@ritm.media>
2026-02-19 02:34:51 +0300

    Check for missing command

--- obfuscator.sh
+++ obfuscator.sh
@@ -2,6 +2,23 @@
 
 set -e
 
+COMMAND_MISSING=false
+if ! command -v git &> /dev/null; then
+  echo "git is not installed"
+  COMMAND_MISSING=true
+fi
+if ! command -v make &> /dev/null; then
+  echo "make is not installed"
+  COMMAND_MISSING=true
+fi
+if ! command -v gcc &> /dev/null; then
+  echo "gcc is not installed"
+  COMMAND_MISSING=true
+fi
+if $COMMAND_MISSING; then
+  exit 1
+fi
+
 git clone https://github.com/ClusterM/wg-obfuscator.git /tmp/wg-obfuscator
 cd /tmp/wg-obfuscator
 make
