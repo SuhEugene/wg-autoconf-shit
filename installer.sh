0c3f0d1e095d696bf7fe6bfab8f769078d7f15aa
Eugene Suharev <e.sukharev@ritm.media>
2026-02-19 00:28:37 +0300

    Comment every action

--- installer.sh
+++ installer.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 
+set -e
+
+echo "Uninstalling..."
 ./uninstaller.sh
+
+echo "Checking ip..."
 
 MY_IP=$1
 if [ -z "$MY_IP" ]; then
@@ -8,7 +13,19 @@ if [ -z "$MY_IP" ]; then
   exit 1
 fi
 
+echo "You want to use $MY_IP as your IP"
+printf "Is that correct? [y/N] "
+read -r answer
+if [[ "$answer" != "y" ]]; then
+  echo "Exiting..."
+  exit 1
+fi
+
+echo "Installing..."
+
 wg genkey > /etc/wireguard/private.key
 ip link add wg0 type wireguard
 ip address add 10.7.7.1/24 dev wg0
 wg set wg0 private-key /etc/wireguard/private.key
+
+echo "Successfully installed!"
--- uninstaller.sh
+++ uninstaller.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
+set -e
+
+echo "Uninstalling..."
+
+echo "Removing wireguard interface..."
 ip link delete wg0 || true
+
+echo "Removing wireguard key..."
 rm /etc/wireguard/private.key || true
+
+echo "Uninstalled!"
